e6ff5c16ba16b9c72dbf0756293c5b3182264b72
Youngseo Lee <107788991+bugerset@users.noreply.github.com>
2026-01-24 17:20:26 +0900

    Update parser.py

--- FedDyn/utils/parser.py
+++ FedDyn/utils/parser.py
@@ -8,7 +8,7 @@ def parse_args():
     parser.add_argument("--device", type=str, default="auto", choices=["auto", "cpu", "cuda", "mps"])
 
     # dataset setting
-    parser.add_argument("--data-set", type=str, default="cifar10", choices=["cifar10, mnist"])
+    parser.add_argument("--data-set", type=str, default="cifar10", choices=["cifar10", "mnist"])
     parser.add_argument("--data-root", type=str, default="./data")
     parser.add_argument("--augment", dest="augment", action="store_true", default=True)
     parser.add_argument("--no-augment", dest="augment", action="store_false")
